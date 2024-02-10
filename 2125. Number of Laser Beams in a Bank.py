4e9c712e11736ea7967de54c9e3119b3e50600bd
LEELA NAGA SAI SATHYENDRANADH NAGASARAPU <114428321+SathyendranadhNagasarapu@users.noreply.github.com>
2024-02-10 15:26:42 +0530

    Q2125 solution

---
+++ 2125. Number of Laser Beams in a Bank.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numberOfBeams(self,bank: List[str]) -> int:
+        beams=0
+        row1=0
+        row2=0
+        for row in bank:
+            if row1==0:
+                row1=row.count("1")
+                continue
+            if row1 and row2:
+                beams+=row1*row2
+                row1=row2
+                row2=0
+            row2=row.count("1")
+        beams+=row1*row2
+        return beams
